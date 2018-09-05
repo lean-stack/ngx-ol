f05b0e8647b82d8a809d4ff3a770e6af2ac1ecaf
Micha Alt <mail@lean-stack.de>
2018-09-05 14:57:40 +0200

    First simple working sample integration.

--- src/app/components/ol/map/map.component.ts
+++ src/app/components/ol/map/map.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+
+import { Map, View } from 'ol';
+
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+import { fromLonLat } from 'ol/proj';
 
 @Component({
   selector: 'ls-map',
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  template: '<div #mapContainer [style.width]="width" [style.height]="height"></div>',
+  styles: [ 'div { border: 1px solid grey; }' ]
 })
 export class MapComponent implements OnInit {
+
+  @Input()
+  width  = '100%';
+
+  @Input()
+  height = '100%';
+
+  @ViewChild('mapContainer')
+  mapContainer: ElementRef;
+
+  private mapInstance: any;
 
   constructor() { }
 
   ngOnInit() {
+
+    this.mapInstance = new Map({
+      target: this.mapContainer.nativeElement,
+      layers: [ new TileLayer({ source: new OSM() }) ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });
+
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const coords = fromLonLat([pos.coords.longitude, pos.coords.latitude]);
+      this.mapInstance.getView().animate({center: coords, zoom: 10});
+    });
   }
 
 }
